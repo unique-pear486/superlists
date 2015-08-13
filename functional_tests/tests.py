3b032049ba875f5f6e70314d2c88a858486408ce
unique-pear486 <unique-pear486@users.noreply.github.com>
2015-08-14 09:40:02 +1000

    First steps of a FT for layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -85,3 +85,25 @@ class NewVisitorTest(LiveServerTestCase):
 
         # Satisfied, they both go back to sleep
         self.assertNotIn('make a fly', page_text)
+
+    def test_layout_and_styling(self):
+        # Edith goes to the home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # She notices that the input box is nicely centred
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=5
+        )
+
+        # She starts a new list and sees that the input is centred there too
+        inputbox.send_keys('testing\n')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=5
+        )
