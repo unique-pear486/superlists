d41db0318d50e8bc3082201dff8eb58f952e4b18
unique-pear486 <unique-pear486@users.noreply.github.com>
2015-08-27 20:01:01 +1000

    Add form for items with existing lists.

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
-from lists.forms import ItemForm, EMPTY_LIST_ERROR
+from lists.forms import ( ItemForm, DUPLICATE_ITEM_ERROR, EMPTY_LIST_ERROR,
+        ExistingListItemForm,)
 from lists.models import Item,List
 
 class ItemFormTest(TestCase):
@@ -13,6 +14,33 @@ class ItemFormTest(TestCase):
         form = ItemForm(data={'text': ''})
         self.assertFalse(form.is_valid())
         self.assertEqual(form.errors['text'], [EMPTY_LIST_ERROR])
+
+    def test_form_handles_saving_to_a_list(self):
+        list_ = List.objects.create()
+        form = ItemForm(data={'text': 'do me'})
+        new_item = form.save(for_list=list_)
+        self.assertEqual(new_item.text, 'do me')
+        self.assertEqual(new_item.list, list_)
+
+class ItemFormTest(TestCase):
+
+    def test_form_renders_item_input(self):
+        list_ = List.objects.create()
+        form = ExistingListItemForm(for_list=list_)
+        self.assertIn('placeholder="Enter a to-do item', form.as_p())
+
+    def test_form_validation_for_blank_items(self):
+        list_ = List.objects.create()
+        form = ExistingListItemForm(for_list=list_, data={'text': ''})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['text'], [EMPTY_LIST_ERROR])
+
+    def test_form_validation_for_duplicate_items(self):
+        list_ = List.objects.create()
+        Item.objects.create(list=list_, text='no twins!')
+        form = ExistingListItemForm(for_list=list_, data={'text': 'no twins!'})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['text'], [DUPLICATE_ITEM_ERROR])
 
     def test_form_handles_saving_to_a_list(self):
         list_ = List.objects.create()
